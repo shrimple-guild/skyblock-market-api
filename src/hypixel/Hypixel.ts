fa3138e2bb5b4f526140a8877e11a11c4820151b
appable <16139460+appable0@users.noreply.github.com>
2025-03-16 21:07:20 -0700

    revert out of scope

--- src/hypixel/Hypixel.ts
+++ src/hypixel/Hypixel.ts
@@ -1,7 +1,5 @@
 import type { ApiSkyblockBazaarResponse } from "../types/ApiSkyblockBazaarResponse"
 import log4js from "log4js"
-import type { ApiSkyblockProfile, ApiSkyblockProfilesJson } from "../types/ApiSkyblockProfilesResponse"
-import { HypixelApiError } from "./HypixelApiError"
 import { Environment } from "../Environment"
 import { Bazaar } from "../bazaar/Bazaar"
 
@@ -13,26 +11,13 @@ async function fetchBazaar(): Promise<Bazaar> {
 	return new Bazaar(bazaarResponse)
 }
 
-async function fetchSkyblockProfiles(uuid: string): Promise<ApiSkyblockProfile[]> {
-	const response = await fetchAuthenticated<ApiSkyblockProfilesJson>("/v2/skyblock/profiles", { uuid: uuid })
-	if (response.profiles == null) {
-		throw new Error(`No profiles found for ${uuid}`)
-	}
-	return response.profiles
-}
-
-async function fetchAuthenticated<T>(endpoint: string, params: SearchParams = {}): Promise<T> {
-	return fetchHypixel(endpoint, { ...params, key: Environment.HYPIXEL_API_KEY })
-}
-
 async function fetchHypixel<T>(endpoint: string, params: SearchParams = {}): Promise<T> {
 	const url = new URL(baseUrl)
 	url.pathname = endpoint
 	url.search = new URLSearchParams(params).toString()
 	const response = await fetch(url)
 	if (response.status != 200) {
-		const safeUrl = url.toString().replace(Environment.HYPIXEL_API_KEY, (key) => key.replace(/[^-]/g, "*"))
-		throw new HypixelApiError(safeUrl, response)
+		throw new Error(`Failed to fetch from ${url.toString()}, ${response.status} ${response.statusText}`)
 	}
 	return response.json()
 }
@@ -40,6 +25,5 @@ async function fetchHypixel<T>(endpoint: string, params: SearchParams = {}): Pro
 type SearchParams = Record<string, string>
 
 export const Hypixel = {
-	fetchBazaar,
-	fetchSkyblockProfiles
+	fetchBazaar
 }
